79966ad4d5aef3e450056568f01ed9dbbb124fa2
Chris Matthews <63919872+cmatthews512@users.noreply.github.com>
2020-05-27 22:03:31 -0400

    Add EF librariescd

--- Scheduler.Database.Core/Models/User.cs
+++ Scheduler.Database.Core/Models/User.cs
@@ -7,12 +7,18 @@ namespace Scheduler.Database.Core.Models
     public class User
     {
         [Key]
+        [Column("id")]
         public int Id { get; set; }
 
+        [Column("first_name")]
         public string FirstName { get; set; }
 
+        [Column("last_name")]
         public string LastName { get; set; }
 
+        [Column("email_addr")]
         public string EmailAddress { get; set; }
+
+        public ICollection<Student> Students { get; set; }
     }
 }
